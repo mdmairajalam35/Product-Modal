4bf83ce0b60245c68fa2ea44ea071fbfd97cc683
misail65 <mdmairajalam35@gmail.com>
2024-02-15 09:23:09 +0530

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
